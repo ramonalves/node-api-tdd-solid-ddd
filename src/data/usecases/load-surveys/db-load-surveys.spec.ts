8a8b339a9b3484328f30e72e61379374db195f1f
ramonsouzasbt <ramonsouza@sbt.com.br>
2021-03-26 22:24:40 -0300

    refactor: change models to use type alias instead of interfaces

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -32,7 +32,7 @@ const makeSurveysRepository = (): LoadSurveysRepository => {
   return new LoadSurveysRepositoryStub()
 }
 
-interface SutTypes {
+type SutTypes = {
   sut: DbLoadSurveys
   loadSurveysRepositoryStub: LoadSurveysRepository
 }
